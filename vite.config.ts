d447ef2a952017689fc233dca7f66434a3f76913
BloodMxxn <bloodm0x0n@gmail.com>
2026-07-11 18:25:47 +0330

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/zing-demo/",
   plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
