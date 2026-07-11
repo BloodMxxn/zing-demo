0d130f928610404bc4169fae879b078dc32524cf
BloodMxxn <bloodm0x0n@gmail.com>
2026-07-11 20:05:04 +0330

    update

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -19,38 +19,36 @@ export default function AuthPage() {
 
   return (
     <div className="max-w-sm">
-      <form>
-        <FieldGroup className="gap-4">
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h1 className="font-morabba text-2xl">ورود به حساب کاربری</h1>
-            <p className="text-sm text-balance text-muted-foreground">
-              برای ورود به اکانت اطلاعات خود را وارد نمایید.
-            </p>
-          </div>
-          <Field>
-            <FieldLabel htmlFor="username">نام کاربری</FieldLabel>
-            <Input
-              className="h-10"
-              dir="ltr"
-              id="username"
-              type="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="jhon_doe"
-              required
-            />
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="password">رمز عبور</FieldLabel>
-            <Input className="h-10" dir="ltr" id="password" type="password" required />
-          </Field>
-          <Field>
-            <Button onClick={clickHandler} size="xl">
-              ورود
-            </Button>
-          </Field>
-        </FieldGroup>
-      </form>
+      <FieldGroup className="gap-4">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="font-morabba text-2xl">ورود به حساب کاربری</h1>
+          <p className="text-sm text-balance text-muted-foreground">
+            برای ورود به اکانت اطلاعات خود را وارد نمایید.
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="username">نام کاربری</FieldLabel>
+          <Input
+            className="h-10"
+            dir="ltr"
+            id="username"
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="jhon_doe"
+            required
+          />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="password">رمز عبور</FieldLabel>
+          <Input className="h-10" dir="ltr" id="password" type="password" required />
+        </Field>
+        <Field>
+          <Button onClick={clickHandler} size="xl">
+            ورود
+          </Button>
+        </Field>
+      </FieldGroup>
     </div>
   );
 }
